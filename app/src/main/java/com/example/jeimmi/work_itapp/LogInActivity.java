9686209ffd58ffa69b2a288669764339939ba324
Jeimmi <jeimmi.gomez@gmail.com>
2016-04-05 20:30:00 -0700

    Updated logIn Activity

--- app/src/main/java/com/example/jeimmi/work_itapp/LogInActivity.java
+++ app/src/main/java/com/example/jeimmi/work_itapp/LogInActivity.java
@@ -1,11 +1,14 @@
 package com.example.jeimmi.work_itapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 import com.amazonaws.regions.Regions;
@@ -14,6 +17,7 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -28,34 +32,29 @@ public class LogInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
         FacebookSdk.sdkInitialize(getApplicationContext());
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_log_in);
+
 
         callbackManager = CallbackManager.Factory.create();
         loginButton =(LoginButton)findViewById(R.id.login_button);
         credentialsProvider = new CognitoCachingCredentialsProvider(
                 this,"us-east-1:a0ab945b-047c-48fb-8c33-9096e0efaeea ", Regions.US_EAST_1);
-        CreateFBCallback ();
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        CreateFBCallback();
+
     }
     private void CreateFBCallback(){
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>(){
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
 
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Map<String,String>Logins = new HashMap<String, String>();
+                Map<String, String> Logins = new HashMap<String, String>();
                 Logins.put("graph.facebook.com", AccessToken.getCurrentAccessToken().getToken());
                 credentialsProvider.setSessionDuration(86400);
                 credentialsProvider.setLogins(Logins);
+
+                Toast toast = Toast.makeText(getApplicationContext(), Profile.getCurrentProfile().getFirstName(), Toast.LENGTH_LONG);
+                toast.show();
             }
 
             @Override
@@ -65,9 +64,17 @@ public class LogInActivity extends AppCompatActivity {
 
             @Override
             public void onError(FacebookException error) {
+                TextView txt = (TextView) findViewById(R.id.info);
+                txt.setText(error.toString());
 
             }
         });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
 
 }
