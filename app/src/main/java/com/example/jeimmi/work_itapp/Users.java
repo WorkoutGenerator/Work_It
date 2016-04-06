314e23b64cd3881c0fbae065e070ea5d1684a6f8
Jeimmi <jeimmi.gomez@gmail.com>
2016-04-05 18:56:16 -0700

    Added the Database object model

---
+++ app/src/main/java/com/example/jeimmi/work_itapp/Users.java
@@ -0,0 +1,44 @@
+package com.example.jeimmi.work_itapp;
+
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBTable;
+
+/**
+ * Created by Jeimmi on 4/5/16.
+ * This class is the Database Table of the User
+ */
+@DynamoDBTable(tableName = "Users")
+public class Users {
+    private String UserID;
+    private String FirstName;
+    private String LastName;
+
+
+    @DynamoDBHashKey(attributeName = "UserID")
+    public String getUserID() {
+
+        return UserID;
+    }
+
+    public void setUserID(String userID) {
+        UserID = userID;
+    }
+    @DynamoDBHashKey(attributeName = "FirstName")
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+    @DynamoDBHashKey(attributeName = "LastName")
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+
+}
