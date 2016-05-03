9ac402297b5fa449edd0aa6f808bb153c4db3404
Jeimmi <jeimmi.gomez@gmail.com>
2016-05-03 00:00:19 -0700

    Attempt to test code to get hashtag in LogInActivity

--- app/src/main/java/com/example/jeimmi/work_itapp/LogInActivity.java
+++ app/src/main/java/com/example/jeimmi/work_itapp/LogInActivity.java
@@ -46,24 +46,24 @@ public class LogInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-//        try {
-//            PackageInfo info = getPackageManager().getPackageInfo(
-//                    "com.hitup.hitup",
-//                    PackageManager.GET_SIGNATURES);
-//            for (Signature signature : info.signatures) {
-//                MessageDigest md = MessageDigest.getInstance("SHA");
-//                md.update(signature.toByteArray());
-//                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-//            }
-//        }
-//        catch (PackageManager.NameNotFoundException e) {
-//
-//        }
-//
-//        catch (NoSuchAlgorithmException e)
-//        {
-//
-//        }
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.hitup.hitup",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        }
+        catch (PackageManager.NameNotFoundException e) {
+
+        }
+
+        catch (NoSuchAlgorithmException e)
+        {
+
+        }
 
 
 
