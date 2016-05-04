9e8ef168217174db60186e30006ee72fe65e8f0c
Jeimmi <jeimmi.gomez@gmail.com>
2016-05-03 22:58:03 -0700

    Small upidates listView continues to fail

--- app/src/main/java/com/example/jeimmi/work_itapp/ListViewWorkOut.java
+++ app/src/main/java/com/example/jeimmi/work_itapp/ListViewWorkOut.java
@@ -1,5 +1,6 @@
 package com.example.jeimmi.work_itapp;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,7 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class ListViewWorkOut extends AppCompatActivity {
+public class ListViewWorkOut extends ListActivity {
     private String workType;
     private Intent incomingIntent;
     private Intent outgoingIntent;
@@ -18,6 +19,7 @@ public class ListViewWorkOut extends AppCompatActivity {
         setContentView(R.layout.activity_list_view_work_out);
 
         start();
+
     }
 
 
@@ -26,46 +28,54 @@ public class ListViewWorkOut extends AppCompatActivity {
         workType = incomingIntent.getStringExtra(WorkItSelector.WORKOUT_TYPE);
         Toast.makeText(ListViewWorkOut.this, "" + workType,
                 Toast.LENGTH_SHORT).show();
-        switch (WorkItSelector.WORKOUT_TYPE){
-            case "ABS":
-                absWork();
-                break;
-            case "ARMS":
-                armsWork();
-                break;
-            case "BACK":
-                backWork();
-                break;
-            case "LEGS":
-                legsWork();
-                break;
+        if(workType.contentEquals("ABS")){
+            absWork();
+
+        }else if(workType.contentEquals("ARMS")){
+            armsWork();
+
+        }else if(workType.contentEquals("BACK")){
+            backWork();
+
+        }else if(workType.contentEquals("LEGS")){
+            legsWork();
+
+        }else{
+
+            Toast.makeText(ListViewWorkOut.this, "NOOOOO BUENO",
+                    Toast.LENGTH_SHORT).show();
+
         }
     }
 
     public void absWork(){
         //Set up and populate the listView
         String absWorking[]={"Double Crunches","Oblique Crunches","Side Bridge"};
-        ArrayAdapter<String> workAdapter = new ArrayAdapter<String>(this, R.layout.activity_work_adapter, absWorking);
         ListView workList = (ListView) findViewById(R.id.WorkOutList);
+        ArrayAdapter<String> workAdapter = new ArrayAdapter<String>(this, R.layout.activity_work_adapter, absWorking);
+
         workList.setAdapter(workAdapter);
     }
     public void armsWork(){
         String armsWorking[]={"Kickbacks","Push Ups","Tri Extensions"};
-        ArrayAdapter<String> workAdapter = new ArrayAdapter<String>(this, R.layout.activity_work_adapter, armsWorking);
         ListView workList = (ListView) findViewById(R.id.WorkOutList);
+        ArrayAdapter<String> workAdapter = new ArrayAdapter<String>(this, R.layout.activity_work_adapter, armsWorking);
+
         workList.setAdapter(workAdapter);
 
     }
     public void legsWork(){
         String legsWorking[]={"Leg Swing","Heel Touch Step Up","Supine Hip"};
-        ArrayAdapter<String> workAdapter = new ArrayAdapter<String>(this, R.layout.activity_work_adapter, legsWorking);
         ListView workList = (ListView) findViewById(R.id.WorkOutList);
+        ArrayAdapter<String> workAdapter = new ArrayAdapter<String>(this, R.layout.activity_work_adapter, legsWorking);
+
         workList.setAdapter(workAdapter);
     }
     public void backWork(){
         String backWorking[]={"Dead Lift","Lying Row","Single Row"};
-        ArrayAdapter<String> workAdapter = new ArrayAdapter<String>(this, R.layout.activity_work_adapter, backWorking);
         ListView workList = (ListView) findViewById(R.id.WorkOutList);
+        ArrayAdapter<String> workAdapter = new ArrayAdapter<String>(this, R.layout.activity_work_adapter, backWorking);
+
         workList.setAdapter(workAdapter);
     }
 
